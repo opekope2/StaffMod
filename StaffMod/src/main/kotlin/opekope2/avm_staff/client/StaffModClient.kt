18fd3cf7dfa47a8295d642a3d071b688e34e7e64
opekope2 <opekope2@gmail.com>
2023-12-28 22:36:05 +0100

    Add client side functionality

---
+++ StaffMod/src/main/kotlin/opekope2/avm_staff/client/StaffModClient.kt
@@ -0,0 +1,63 @@
+package opekope2.avm_staff.client
+
+import net.fabricmc.api.ClientModInitializer
+import net.fabricmc.api.EnvType
+import net.fabricmc.api.Environment
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper
+import net.fabricmc.fabric.api.client.model.loading.v1.ModelLoadingPlugin
+import net.fabricmc.fabric.api.client.model.loading.v1.ModelModifier
+import net.minecraft.client.MinecraftClient
+import net.minecraft.client.option.KeyBinding
+import net.minecraft.client.render.model.BakedModel
+import net.minecraft.client.util.InputUtil
+import opekope2.avm_staff.StaffMod.MOD_ID
+import opekope2.avm_staff.model.StaffItemModel
+import opekope2.avm_staff.packet.AddBlockToStaffC2SPacket
+import opekope2.avm_staff.packet.RemoveBlockFromStaffC2SPacket
+import opekope2.avm_staff.util.hasBlock
+import org.lwjgl.glfw.GLFW
+
+@Suppress("unused")
+@Environment(EnvType.CLIENT)
+object StaffModClient : ClientModInitializer {
+    @JvmStatic
+    val ADD_REMOVE_KEYBIND = KeyBindingHelper.registerKeyBinding(
+        KeyBinding(
+            "key.avm_staff.add_remove_staff_block",
+            InputUtil.Type.KEYSYM,
+            GLFW.GLFW_KEY_R,
+            "key.categories.avm_staff"
+        )
+    )
+
+    override fun onInitializeClient() {
+        ModelLoadingPlugin.register(::modelLoadingPlugin)
+        ClientTickEvents.END_CLIENT_TICK.register(::handleStaffKeybind)
+    }
+
+    private fun modelLoadingPlugin(pluginContext: ModelLoadingPlugin.Context) {
+        pluginContext.modifyModelAfterBake().register(::modifyModelAfterBake)
+    }
+
+    private fun modifyModelAfterBake(model: BakedModel?, context: ModelModifier.AfterBake.Context): BakedModel? {
+        val id = context.id()
+
+        return if (model == null || id.namespace != MOD_ID || id.path != "staff") model
+        else StaffItemModel(model)
+    }
+
+    private fun handleStaffKeybind(client: MinecraftClient) {
+        if (ADD_REMOVE_KEYBIND.isPressed) {
+            ADD_REMOVE_KEYBIND.isPressed = false
+
+            val player = client.player ?: return
+
+            if (player.mainHandStack.hasBlock || player.offHandStack.hasBlock) {
+                RemoveBlockFromStaffC2SPacket().send()
+            } else {
+                AddBlockToStaffC2SPacket().send()
+            }
+        }
+    }
+}
