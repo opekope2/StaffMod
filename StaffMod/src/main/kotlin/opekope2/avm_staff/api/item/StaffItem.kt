f70923d7689ae37f5d93a901d5825a4bc76639e3
opekope2 <opekope2@gmail.com>
2024-01-05 22:12:49 +0100

    Add translation for staff with item added to it

--- StaffMod/src/main/kotlin/opekope2/avm_staff/api/item/StaffItem.kt
+++ StaffMod/src/main/kotlin/opekope2/avm_staff/api/item/StaffItem.kt
@@ -4,8 +4,23 @@
 package opekope2.avm_staff.api.item
 
 import net.minecraft.item.Item
+import net.minecraft.item.ItemStack
+import net.minecraft.text.Text
+import opekope2.avm_staff.util.isItemInStaff
+import opekope2.avm_staff.util.itemInStaff
 
 /**
  * Staff item.
  */
-class StaffItem(settings: Settings) : Item(settings)
+class StaffItem(settings: Settings) : Item(settings) {
+    override fun getName(stack: ItemStack): Text {
+        val staffItem = stack.itemInStaff ?: return super.getName(stack)
+        val staffItemText = Text.translatable(staffItem.item.getTranslationKey(staffItem))
+        return Text.translatable(getTranslationKey(stack), staffItemText)
+    }
+
+    override fun getTranslationKey(stack: ItemStack): String {
+        return if (stack.isItemInStaff) "$translationKey.with_item"
+        else super.getTranslationKey(stack)
+    }
+}
