d9de79a20b9fa0d668bd584fbfe6ebeb6836467f
opekope2 <opekope2@gmail.com>
2024-01-05 22:15:31 +0100

    Throw item out of staff if destroyed

--- StaffMod/src/main/kotlin/opekope2/avm_staff/api/item/StaffItem.kt
+++ StaffMod/src/main/kotlin/opekope2/avm_staff/api/item/StaffItem.kt
@@ -5,14 +5,22 @@ package opekope2.avm_staff.api.item
 
 import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
+import net.minecraft.item.ItemUsage
 import net.minecraft.text.Text
 import opekope2.avm_staff.util.isItemInStaff
 import opekope2.avm_staff.util.itemInStaff
+import java.util.stream.Stream
 
 /**
  * Staff item.
  */
 class StaffItem(settings: Settings) : Item(settings) {
+    override fun onItemEntityDestroyed(entity: ItemEntity) {
+        val staffStack = entity.stack
+        val staffItem = staffStack.itemInStaff ?: return
+        ItemUsage.spawnItemContents(entity, Stream.of(staffItem))
+    }
+
     override fun getName(stack: ItemStack): Text {
         val staffItem = stack.itemInStaff ?: return super.getName(stack)
         val staffItemText = Text.translatable(staffItem.item.getTranslationKey(staffItem))
