a68aa233dba383a3262ed3ea640fbdf366297e91
opekope2 <opekope2@gmail.com>
2024-02-07 20:28:28 +0100

    Remove IStaffMod field from LivingEntityMixin
    It crashes Forge

--- StaffMod/src/main/java/opekope2/avm_staff/mixin/LivingEntityMixin.java
+++ StaffMod/src/main/java/opekope2/avm_staff/mixin/LivingEntityMixin.java
@@ -26,23 +26,19 @@ import opekope2.avm_staff.api.item.StaffItemHandler;
 import opekope2.avm_staff.util.StaffUtil;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin {
-    @Unique
-    private static final IStaffMod staffMod$impl = IStaffMod.get();
-
     @Shadow
     public abstract ItemStack getMainHandStack();
 
     @Inject(method = "disablesShield", at = @At("HEAD"), cancellable = true)
     public void disableShield(CallbackInfoReturnable<Boolean> cir) {
         ItemStack mainHandStack = getMainHandStack();
-        if (!mainHandStack.isOf(staffMod$impl.getStaffItem())) return;
+        if (!mainHandStack.isOf(IStaffMod.get().getStaffItem())) return;
 
         ItemStack itemInStaff = StaffUtil.getItemInStaff(mainHandStack);
         if (itemInStaff == null) return;
