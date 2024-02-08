ddc082739964cf87a7a981230af7be2c26b2eaad
opekope2 <opekope2@gmail.com>
2024-02-08 14:49:08 +0100

    Local variable capture stopped working

--- StaffMod/src/main/java/opekope2/avm_staff/mixin/MinecraftClientMixin.java
+++ StaffMod/src/main/java/opekope2/avm_staff/mixin/MinecraftClientMixin.java
@@ -29,14 +29,11 @@ import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.util.hit.HitResult;
-import opekope2.avm_staff.IStaffMod;
-import opekope2.avm_staff.api.item.StaffItemHandler;
+import opekope2.avm_staff.internal.event_handler.StaffAttackHandler;
 import opekope2.avm_staff.internal.networking.c2s.play.StaffAttackC2SPacket;
-import opekope2.avm_staff.util.StaffUtil;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -46,9 +43,6 @@ import java.util.Objects;
 
 @Mixin(MinecraftClient.class)
 public abstract class MinecraftClientMixin {
-    @Unique
-    private static final IStaffMod staffMod$impl = IStaffMod.get();
-
     @Shadow
     @Nullable
     public ClientPlayerEntity player;
@@ -75,34 +69,26 @@ public abstract class MinecraftClientMixin {
             locals = LocalCapture.CAPTURE_FAILHARD,
             cancellable = true
     )
-    private void handleEntityAttack(CallbackInfoReturnable<Boolean> cir, ItemStack itemStack, boolean bl) {
+    private void handleEntityAttack(CallbackInfoReturnable<Boolean> cir) {
         assert player != null;
         assert world != null;
         assert crosshairTarget != null;
 
-        if (!itemStack.isOf(staffMod$impl.getStaffItem())) return;
-
-        ItemStack itemInStaff = StaffUtil.getItemInStaff(itemStack);
-        if (itemInStaff == null) return;
-        StaffItemHandler staffHandler = StaffUtil.getHandlerOfItem(itemInStaff);
-        if (staffHandler == null) return;
-
         Entity target = ((EntityHitResult) crosshairTarget).getEntity();
-
-        ActionResult result = staffHandler.attackEntity(itemStack, world, player, target, Hand.MAIN_HAND);
+        ActionResult result = StaffAttackHandler.attackEntity(player, world, Hand.MAIN_HAND, target);
         switch (result) {
             case SUCCESS -> {
                 Objects.requireNonNull(getNetworkHandler())
                         .sendPacket(PlayerInteractEntityC2SPacket.attack(target, player.isSneaking()));
                 player.swingHand(Hand.MAIN_HAND);
-                cir.setReturnValue(bl);
+                cir.setReturnValue(false);
             }
             case CONSUME, CONSUME_PARTIAL -> {
                 Objects.requireNonNull(getNetworkHandler())
                         .sendPacket(PlayerInteractEntityC2SPacket.attack(target, player.isSneaking()));
-                cir.setReturnValue(bl);
+                cir.setReturnValue(false);
             }
-            case FAIL -> cir.setReturnValue(bl);
+            case FAIL -> cir.setReturnValue(false);
         }
     }
 
@@ -113,32 +99,26 @@ public abstract class MinecraftClientMixin {
                     target = "Lnet/minecraft/client/network/ClientPlayerInteractionManager;hasLimitedAttackSpeed()Z",
                     ordinal = 1
             ),
-            locals = LocalCapture.CAPTURE_FAILHARD,
             cancellable = true
     )
-    private void handleAttack(CallbackInfoReturnable<Boolean> cir, ItemStack itemStack, boolean bl) {
+    private void handleAttack(CallbackInfoReturnable<Boolean> cir) {
         assert player != null;
         assert world != null;
 
-        if (!itemStack.isOf(staffMod$impl.getStaffItem())) return;
+        ItemStack stackInHand = player.getStackInHand(Hand.MAIN_HAND);
 
-        ItemStack itemInStaff = StaffUtil.getItemInStaff(itemStack);
-        if (itemInStaff == null) return;
-        StaffItemHandler staffHandler = StaffUtil.getHandlerOfItem(itemInStaff);
-        if (staffHandler == null) return;
-
-        ActionResult result = staffHandler.attack(itemStack, world, player, Hand.MAIN_HAND);
+        ActionResult result = StaffAttackHandler.attack(stackInHand, world, player, Hand.MAIN_HAND);
         switch (result) {
             case SUCCESS -> {
                 new StaffAttackC2SPacket().send();
                 player.swingHand(Hand.MAIN_HAND);
-                cir.setReturnValue(bl);
+                cir.setReturnValue(false);
             }
             case CONSUME, CONSUME_PARTIAL -> {
                 new StaffAttackC2SPacket().send();
-                cir.setReturnValue(bl);
+                cir.setReturnValue(false);
             }
-            case FAIL -> cir.setReturnValue(bl);
+            case FAIL -> cir.setReturnValue(false);
         }
     }
 }
