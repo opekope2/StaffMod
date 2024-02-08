e25d6f73d4560948c591d77554d515d4815143a6
opekope2 <opekope2@gmail.com>
2024-02-08 02:48:33 +0100

    Move key binding handling to common project

--- FabricMod/src/main/kotlin/opekope2/avm_staff/internal/fabric/StaffModClient.kt
+++ FabricMod/src/main/kotlin/opekope2/avm_staff/internal/fabric/StaffModClient.kt
@@ -25,33 +25,21 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper
 import net.fabricmc.fabric.api.client.model.loading.v1.ModelLoadingPlugin
 import net.fabricmc.fabric.api.client.model.loading.v1.ModelModifier
-import net.minecraft.client.MinecraftClient
-import net.minecraft.client.option.KeyBinding
 import net.minecraft.client.render.model.BakedModel
-import net.minecraft.client.util.InputUtil
+import opekope2.avm_staff.internal.event_handler.ADD_REMOVE_KEYBINDING
 import opekope2.avm_staff.internal.fabric.item.model.StaffItemModel
-import opekope2.avm_staff.internal.networking.c2s.play.AddItemToStaffC2SPacket
-import opekope2.avm_staff.internal.networking.c2s.play.RemoveItemFromStaffC2SPacket
+import opekope2.avm_staff.internal.event_handler.handleKeyBindings
 import opekope2.avm_staff.util.MOD_ID
-import opekope2.avm_staff.util.isItemInStaff
-import org.lwjgl.glfw.GLFW
 
 @Suppress("unused")
 @Environment(EnvType.CLIENT)
 object StaffModClient : ClientModInitializer {
-    @JvmField
-    val ADD_REMOVE_KEYBINDING: KeyBinding = KeyBindingHelper.registerKeyBinding(
-        KeyBinding(
-            "key.avm_staff.add_remove_staff_block",
-            InputUtil.Type.KEYSYM,
-            GLFW.GLFW_KEY_R,
-            "key.categories.avm_staff"
-        )
-    )
-
     override fun onInitializeClient() {
         ModelLoadingPlugin.register(::modelLoadingPlugin)
-        ClientTickEvents.END_CLIENT_TICK.register(::handleStaffKeybinding)
+
+        KeyBindingHelper.registerKeyBinding(ADD_REMOVE_KEYBINDING)
+
+        ClientTickEvents.END_CLIENT_TICK.register(::handleKeyBindings)
     }
 
     private fun modelLoadingPlugin(pluginContext: ModelLoadingPlugin.Context) {
@@ -63,19 +51,5 @@ object StaffModClient : ClientModInitializer {
 
         return if (model == null || id.namespace != MOD_ID || id.path != "staff") model
         else StaffItemModel(model)
-    }
-
-    private fun handleStaffKeybinding(client: MinecraftClient) {
-        if (ADD_REMOVE_KEYBINDING.isPressed) {
-            ADD_REMOVE_KEYBINDING.isPressed = false
-
-            val player = client.player ?: return
-
-            if (player.mainHandStack.isItemInStaff || player.offHandStack.isItemInStaff) {
-                RemoveItemFromStaffC2SPacket().send()
-            } else {
-                AddItemToStaffC2SPacket().send()
-            }
-        }
     }
 }
