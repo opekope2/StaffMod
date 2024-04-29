240dd07fb2980e02b682b09eaea474a33b47ac2e
opekope2 <opekope2@gmail.com>
2024-04-29 16:26:27 +0200

    Fix Fabric copyright messages because IntelliJ overwrote it

--- NeoForgeMod/src/main/java/opekope2/avm_staff/mixin/neoforge/ServerPlayerInteractionManagerMixin.java
+++ NeoForgeMod/src/main/java/opekope2/avm_staff/mixin/neoforge/ServerPlayerInteractionManagerMixin.java
@@ -1,19 +1,17 @@
 /*
- * AvM Staff Mod
- * Copyright (c) 2016-2024 opekope2
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
  *
- * This mod is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This mod is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Lesser General Public License for more details.
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this mod. If not, see <https://www.gnu.org/licenses/>.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package opekope2.avm_staff.mixin.neoforge;
