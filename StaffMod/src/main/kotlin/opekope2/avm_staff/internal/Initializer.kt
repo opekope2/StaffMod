41983293368f14ed958d6a595a6928ed949939fd
opekope2 <opekope2@gmail.com>
2024-05-20 01:49:27 +0200

    Extract modifyLootTables from subscribeToEvents

--- StaffMod/src/main/kotlin/opekope2/avm_staff/internal/Initializer.kt
+++ StaffMod/src/main/kotlin/opekope2/avm_staff/internal/Initializer.kt
@@ -20,6 +20,7 @@ package opekope2.avm_staff.internal
 
 import dev.architectury.event.events.common.InteractionEvent
 import dev.architectury.event.events.common.LootEvent
+import net.minecraft.loot.LootManager
 import net.minecraft.loot.LootPool
 import net.minecraft.loot.entry.ItemEntry
 import net.minecraft.util.Identifier
@@ -41,12 +42,20 @@ fun initializeNetworking() {
 
 private val TREASURE_BASTION_CHEST_LOOT = Identifier("chests/bastion_treasure")
 
+@Suppress("UNUSED_PARAMETER")
+fun modifyLootTables(
+    lootManager: LootManager?,
+    lootTableId: Identifier,
+    context: LootEvent.LootTableModificationContext,
+    builtin: Boolean
+) {
+    if (builtin && lootTableId == TREASURE_BASTION_CHEST_LOOT) {
+        context.addPool(LootPool.builder().with(ItemEntry.builder(crownOfKingOrangeItem.get())))
+    }
+}
+
 fun subscribeToEvents() {
     InteractionEvent.LEFT_CLICK_BLOCK.register(::attackBlock)
     InteractionEvent.CLIENT_LEFT_CLICK_AIR.register(::clientAttack)
-    LootEvent.MODIFY_LOOT_TABLE.register(LootEvent.ModifyLootTable { _, lootTableId, context, builtin ->
-        if (builtin && lootTableId == TREASURE_BASTION_CHEST_LOOT) {
-            context.addPool(LootPool.builder().with(ItemEntry.builder(crownOfKingOrangeItem.get())))
-        }
-    })
+    LootEvent.MODIFY_LOOT_TABLE.register(::modifyLootTables)
 }
