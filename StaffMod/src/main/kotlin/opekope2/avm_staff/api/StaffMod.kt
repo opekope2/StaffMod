b65890f3484a9cc86ccbd5664669e777b37c8773
opekope2 <opekope2@gmail.com>
2024-11-11 01:37:09 +0100

    Re-add avm_staff:staffs tag

--- StaffMod/src/main/kotlin/opekope2/avm_staff/api/StaffMod.kt
+++ StaffMod/src/main/kotlin/opekope2/avm_staff/api/StaffMod.kt
@@ -41,6 +41,7 @@ import net.minecraft.network.codec.PacketCodec
 import net.minecraft.particle.SimpleParticleType
 import net.minecraft.registry.RegistryKey
 import net.minecraft.registry.RegistryKeys
+import net.minecraft.registry.tag.TagKey
 import net.minecraft.sound.BlockSoundGroup
 import net.minecraft.sound.SoundEvent
 import net.minecraft.text.Text
@@ -162,6 +163,11 @@ val staffInfusionSmithingTemplateItem: RegistrySupplier<Item> = ITEMS.register("
         StaffInfusionSmithingRecipeTextures.additionsSlotTextures
     )
 }
+
+/**
+ * Tag registered as `avm_staff:staffs`.
+ */
+val staffsTag: TagKey<Item> = TagKey.of(RegistryKeys.ITEM, Identifier.of(MOD_ID, "staffs"))
 
 /**
  * Item group containing items added by Staff Mod.
