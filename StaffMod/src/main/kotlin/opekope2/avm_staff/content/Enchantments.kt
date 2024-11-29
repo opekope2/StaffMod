c69e2616891aee3e7590ba812a0ac9d00034e1df
opekope2 <opekope2@gmail.com>
2024-11-29 21:02:21 +0100

    Add quick draw enchantment

--- StaffMod/src/main/kotlin/opekope2/avm_staff/content/Enchantments.kt
+++ StaffMod/src/main/kotlin/opekope2/avm_staff/content/Enchantments.kt
@@ -29,6 +29,9 @@ import opekope2.avm_staff.util.TagKeyUtil
  */
 object Enchantments : RegistryKeyUtil<Enchantment>(MOD_ID, RegistryKeys.ENCHANTMENT) {
     @JvmField
+    val QUICK_DRAW = registryKey("quick_draw")
+
+    @JvmField
     val RAPID_FIRE = registryKey("rapid_fire")
 
     @JvmField
