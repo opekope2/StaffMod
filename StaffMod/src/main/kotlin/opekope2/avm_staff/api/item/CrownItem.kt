1bee88a1ad68d5258c2d81728110c9e06d78c217
opekope2 <opekope2@gmail.com>
2025-06-21 02:18:38 +0200

    Disable placing enchanted crowns

--- StaffMod/src/main/kotlin/opekope2/avm_staff/api/item/CrownItem.kt
+++ StaffMod/src/main/kotlin/opekope2/avm_staff/api/item/CrownItem.kt
@@ -1,6 +1,6 @@
 /*
  * AvM Staff Mod
- * Copyright (c) 2024 opekope2
+ * Copyright (c) 2024-2025 opekope2
  *
  * This mod is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -20,12 +20,10 @@ package opekope2.avm_staff.api.item
 
 import net.minecraft.block.Block
 import net.minecraft.block.DispenserBlock
+import net.minecraft.enchantment.EnchantmentHelper
 import net.minecraft.entity.EquipmentSlot
 import net.minecraft.entity.player.PlayerEntity
-import net.minecraft.item.ArmorItem
-import net.minecraft.item.Equipment
-import net.minecraft.item.ItemStack
-import net.minecraft.item.VerticallyAttachableBlockItem
+import net.minecraft.item.*
 import net.minecraft.registry.entry.RegistryEntry
 import net.minecraft.sound.SoundEvent
 import net.minecraft.sound.SoundEvents
@@ -54,4 +52,8 @@ open class CrownItem(groundBlock: Block, wallBlock: Block, settings: Settings) :
     override fun getEquipSound(): RegistryEntry<SoundEvent> = SoundEvents.ITEM_ARMOR_EQUIP_GOLD
 
     override fun getSlotType(): EquipmentSlot = EquipmentSlot.HEAD
+
+    override fun getPlacementState(context: ItemPlacementContext) =
+        if (EnchantmentHelper.hasEnchantments(context.stack)) null
+        else super.getPlacementState(context)
 }
